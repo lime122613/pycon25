e1b5cb189e606bc654a9878564e603b5ca5a1ccb
lime122613 <167674932+lime122613@users.noreply.github.com>
2025-08-15 07:42:55 +0000

    문구 수정

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,15 @@
 import streamlit as st
 tab1, tab2, tab3 = st.tabs(["사진", "공유 시트 보기","공유 시트 쓰기"])  # 2개의 탭 생성
 with tab1:
-    st.title("😊 자모옹의 파이콘 튜토리얼 💕")
+    st.title("😊 자모옹의 첫 번째 앱 💕")
     st.info("안녕하세요~! 파이콘 튜토리얼 예제 앱입니다!")
 
-    st.subheader("첫 번째 앱")
+    st.subheader("사진첩")
     st.write(
         "내가 좋아하는 동물을 소개해봅시다!"
     )
     st.image("https://img.animalplanet.co.kr/news/2022/10/13/700/gzs211818b42g2a88a13.jpg", caption="행복한 쿼카", use_container_width=True)
 
-    # 코드를 화면에 보이고 싶으면
-    st.code("""
-    import streamlit as st
-    st.title('Hello World')
-    """, language="python")
-
-    st.write('https://naver.com')
     st.link_button("네이버 바로가기", 'https://naver.com')
 
 
