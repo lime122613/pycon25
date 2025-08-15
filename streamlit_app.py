1316685a5b22cc99a56d2c32d1a226bd7af5680f
lime122613 <167674932+lime122613@users.noreply.github.com>
2025-08-15 07:14:58 +0000

    2단계 보안

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
-st.title("🎈 😊자모옹의 파이콘 튜토리얼💕")
+st.title("😊 자모옹의 파이콘 튜토리얼 💕")
 st.info("안녕하세요~! 파이콘 튜토리얼 예제 앱입니다!")
 
 st.subheader("첫 번째 앱")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "내가 좋아하는 동물을 소개해봅시다!"
 )
 st.image("https://img.animalplanet.co.kr/news/2022/10/13/700/gzs211818b42g2a88a13.jpg", caption="행복한 쿼카", use_container_width=True)
 
+# 코드를 화면에 보이고 싶으면
 st.code("""
 import streamlit as st
 st.title('Hello World')
